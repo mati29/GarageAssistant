e021b30bf90933d67bc77fa55bd6b2abbad36fb4
mati29 <mering20@gmail.com>
2017-03-12 12:51:27 +0100

    Add settings only for client
    little bug reveal but already fixed
    +maybe in future some option for employee or admin will be added

--- src/main/java/CarPackage/AfterLogging.java
+++ src/main/java/CarPackage/AfterLogging.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Collection;
 
 /**
  * Created by Mati on 2017-03-11.
@@ -20,16 +22,19 @@ public class AfterLogging implements ApplicationListener<AuthenticationSuccessEv
         private AccountRepository accountRepository;
         @Override
         public void onApplicationEvent(AuthenticationSuccessEvent event) {
-            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
-            Object principal = token.getPrincipal();
-            if(principal instanceof User) {
-                User user = (User) token.getPrincipal();
-                Account account = accountRepository.findByUsername(user.getUsername());
-                Settings settings = account.getClient().getSettings();
-                session.setAttribute("AP", settings.getAutoPart());
-                session.setAttribute("AM", settings.getAutoMechanic());
-                session.setAttribute("AS", settings.getAdditionalService());
-                session.setAttribute("EP", settings.getCallExtraPart());
+            SimpleGrantedAuthority authority = (SimpleGrantedAuthority)event.getAuthentication().getAuthorities().iterator().next();
+            if("ROLE_USER".equals(authority.getAuthority())){
+                UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+                Object principal = token.getPrincipal();
+                if (principal instanceof User) {
+                    User user = (User) token.getPrincipal();
+                    Account account = accountRepository.findByUsername(user.getUsername());
+                    Settings settings = account.getClient().getSettings();
+                    session.setAttribute("AP", settings.getAutoPart());
+                    session.setAttribute("AM", settings.getAutoMechanic());
+                    session.setAttribute("AS", settings.getAdditionalService());
+                    session.setAttribute("EP", settings.getCallExtraPart());
+                }
             }
         }
 
