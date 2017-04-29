6f583498f8f339eef770232f09da431835dd866f
mati29 <mering20@gmail.com>
2017-04-29 11:22:09 +0200

    Change Main Adnotation and resolve register bug

--- src/main/java/CarPackage/Application.java
+++ src/main/java/CarPackage/Application.java
@@ -5,10 +5,10 @@ package main.java.CarPackage;
  */
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
